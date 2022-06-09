a463d8bf2f59639520ee2da7fb21037396d2a721
sjt <mr.songjintao@gmail.com>
2022-06-09 16:54:41 +0800

    app.vue

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>{{count}}</h1>
+    <p1>{{count}}</p1>
+    <button @click="add">增加</button>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "App",
+  data() {
+    return {
+    count:0
+    };
+  },
+  methods: {
+    add() {
+        this.count = ++this.count
+    },
+  }
+};
+</script>
+<style>
+</style>
